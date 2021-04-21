972bdda10c23e9e1cdc454c663d116384bebbf94
MiroslavHubenko <miroslavgubenko@gmail.com>
2021-04-21 21:40:51 +0300

    init | added : theme swither | save theme in LC

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,58 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="root_container">
+    <div class="top_bar">
+      <img class="logo" src="./assets/logo.svg" alt="logo" />
+      <a href="#"
+        ><img class="githublogo" src="./assets/github_logo.png" alt="gitlogo" />
+      </a>
+    </div>
+    <div class="color_piker">
+      <ul v-for="(color, i) in colors" :key="i">
+        <li @click="SetTheme(color)" :style="{ backgroundColor: color }"></li>
+      </ul>
+    </div>
+    <div class="container_center">
+      <h1>Список заметок пуст</h1>
+      <button class="add_note">Добавить заметку</button>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
   name: "App",
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      colors: [
+        "#773a9c",
+        "#3a619c",
+        "#9c3a3a",
+        "#3a9c85",
+        "#FFA07A",
+        "#778899",
+      ],
+    };
+  },
+  mounted() {
+    if (localStorage.getItem("theme")) {
+      document.documentElement.setAttribute(
+        "theme",
+        localStorage.getItem("theme")
+      );
+    } else {
+      console.log(localStorage.getItem("theme"));
+      localStorage.setItem("theme", "#773a9c");
+    }
+  },
+  methods: {
+    SetTheme: function (color) {
+      localStorage.setItem("theme", color);
+      document.documentElement.setAttribute("theme", color);
+    },
   },
 };
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style>
+@import url("https://fonts.googleapis.com/css2?family=PT+Sans&display=swap");
 </style>
